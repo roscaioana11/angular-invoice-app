5bf6d85bf88c2868a20a10514d5c9eb727c794cd
Ioana Rosca <rosca.ioana11@gmail.com>
2021-07-09 22:38:48 +0300

    Angular Setup

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {InvoiceService} from "./service/invoice.service";
+import { InvoiceComponent } from './invoice/invoice.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InvoiceComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [InvoiceService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
