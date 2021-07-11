6655d7c0d24431b8f64f49d5aab6c074994969be
Ioana Rosca <rosca.ioana11@gmail.com>
2021-07-11 03:32:39 +0300

    PUT request

--- src/app/invoice/invoice.component.ts
+++ src/app/invoice/invoice.component.ts
@@ -11,9 +11,12 @@ import {FormBuilder} from "@angular/forms";
 })
 export class InvoiceComponent implements OnInit {
 
+  isAddMode: boolean | undefined;
+
   closeResult = '';
   invoiceList: Invoice[] = []; //declara lista care v-a popula tabelul
   myForm = this.formBuilder.group({
+    id:'',
     description: '',
     amount: '',
     sender: '',
@@ -23,6 +26,8 @@ export class InvoiceComponent implements OnInit {
   constructor(private httpClient: HttpClient, private modalService: NgbModal, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.isAddMode = false;
+
     this.httpClient.get<Invoice[]>('http://localhost:8081/invoices').subscribe(response => {
       console.log(response);
       this.invoiceList = response;
@@ -61,4 +66,31 @@ export class InvoiceComponent implements OnInit {
     })
   }
 
+  edit(){
+    const invoice: Invoice = {amount: this.myForm.value.amount, description: this.myForm.value.description,
+      receiver: this.myForm.value.receiver, sender: this.myForm.value.sender}
+    this.httpClient.put('http://localhost:8081/invoices/' + this.myForm.value.id, invoice).subscribe(response => {
+      const editElement = this.invoiceList.find(element => element.id == this.myForm.value.id);
+      if (editElement) {
+        const index = this.invoiceList.indexOf(editElement)
+        this.invoiceList[index] = <Invoice>response;
+      }
+    })
+  }
+
+  openEdit(content: any, existingInvoice: Invoice){
+    this.myForm.setValue(
+      {
+        id: existingInvoice.id,
+        description: existingInvoice.description,
+        amount: existingInvoice.amount,
+        receiver: existingInvoice.receiver,
+        sender: existingInvoice.sender
+      })
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
 }
